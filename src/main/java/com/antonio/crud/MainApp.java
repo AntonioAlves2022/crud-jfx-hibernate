4b8c3f9d5b73d9c9e229035bcf342a6e3a474a07
Antonio A Alves <antonio.alves@duque.g12.br>
2025-07-24 15:04:08 -0300

    Aula 05 - Testando a tela de carregamento

--- src/main/java/com/antonio/crud/MainApp.java
+++ src/main/java/com/antonio/crud/MainApp.java
@@ -4,18 +4,60 @@
  */
 package com.antonio.crud;
 
+import com.antonio.crud.controllers.SplashController;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author Professor
  */
 public class MainApp extends Application{
-
+    private Stage primaryStage;
     @Override
-    public void start(Stage stage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void start(Stage primaryStage)  {
+        this.primaryStage = primaryStage;
+        showSplashScreen();
+    }
+    
+    private void showSplashScreen(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/splash-screen.fxml"));
+            Parent root = loader.load();
+            Stage splashStage = new Stage();
+            splashStage.initStyle(StageStyle.UNDECORATED);
+            splashStage.setScene(new Scene(root));
+            splashStage.show();
+            
+            //Configurando o Controller
+            SplashController controller = loader.getController();
+            //Simular o carregamento
+            new Thread(()->{
+                try{
+                 for(int i = 0; i <= 100; i++){
+                     final int progress = i;
+                     Platform.runLater(()->{
+                         controller.setProgress(progress / 100);
+                         controller.setMessages("Carregando "+ progress + "%");
+                     });
+                     Thread.sleep(30);
+                 } 
+                 Platform.runLater(()->{
+                 splashStage.close();
+                 });
+                }catch(InterruptedException ex){
+                    Thread.currentThread().interrupt();
+                }
+            }).start();
+        }catch(Exception ex){
+            ex.printStackTrace();
+            
+        }
     }
     
     public static void main(String[] args) {
