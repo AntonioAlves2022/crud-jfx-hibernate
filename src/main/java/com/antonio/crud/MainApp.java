f8312ab6de6ec947a9c935c1923f736bd3941506
Antonio A Alves <antonio.alves@duque.g12.br>
2025-07-24 14:37:07 -0300

    Aula 05 - Criando a tela de menu e controller principal

---
+++ src/main/java/com/antonio/crud/MainApp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.antonio.crud;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Professor
+ */
+public class MainApp extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
