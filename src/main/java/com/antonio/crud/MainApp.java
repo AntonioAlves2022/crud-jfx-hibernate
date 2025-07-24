8878d5d69fd43d83f9c0220555db0ed39cde09dd
Antonio A Alves <antonio.alves@duque.g12.br>
2025-07-24 15:53:43 -0300

    Aula 07 - Abrindo a tela de login apos o loading

--- src/main/java/com/antonio/crud/MainApp.java
+++ src/main/java/com/antonio/crud/MainApp.java
@@ -4,6 +4,7 @@
  */
 package com.antonio.crud;
 
+import com.antonio.crud.controllers.LoginController;
 import com.antonio.crud.controllers.SplashController;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -17,51 +18,73 @@ import javafx.stage.StageStyle;
  *
  * @author Professor
  */
-public class MainApp extends Application{
+public class MainApp extends Application {
+
     private Stage primaryStage;
+
     @Override
-    public void start(Stage primaryStage)  {
+    public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         showSplashScreen();
     }
-    
-    private void showSplashScreen(){
-        try{
+
+    private void showSplashScreen() {
+
+        try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/splash-screen.fxml"));
             Parent root = loader.load();
             Stage splashStage = new Stage();
             splashStage.initStyle(StageStyle.UNDECORATED);
             splashStage.setScene(new Scene(root));
             splashStage.show();
-            
+
             //Configurando o Controller
             SplashController controller = loader.getController();
             //Simular o carregamento
-            new Thread(()->{
-                try{
-                 for(int i = 0; i <= 100; i++){
-                     final int progress = i;
-                     Platform.runLater(()->{
-                         controller.setProgress(progress / 100);
-                         controller.setMessages("Carregando "+ progress + "%");
-                     });
-                     Thread.sleep(30);
-                 } 
-                 Platform.runLater(()->{
-                 splashStage.close();
-                 });
-                }catch(InterruptedException ex){
+            new Thread(() -> {
+                try {
+                    for (int i = 0; i <= 100; i++) {
+                        final int progress = i;
+                        Platform.runLater(() -> {
+                            controller.setProgress(progress / 100);
+                            controller.setMessages("Carregando " + progress + "%");
+                        });
+                        Thread.sleep(30);
+                    }
+                    Platform.runLater(() -> {
+                        splashStage.close();
+                        showLoginScreen();
+                    });
+                } catch (InterruptedException ex) {
                     Thread.currentThread().interrupt();
                 }
             }).start();
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
-            
+
         }
     }
-    
+
+    private void showLoginScreen() {
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/tela-login.fxml"));
+        Parent root = loader.load();
+        Stage loginStage = new Stage();
+        loginStage.setTitle("Tela de login");
+        loginStage.setScene(new Scene(root));
+        loginStage.setResizable(false);
+        LoginController controller = loader.getController();
+        controller.setDialogStage(loginStage);
+        loginStage.showAndWait();
+       
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
--- src/main/java/com/antonio/crud/controllers/LoginController.java
+++ src/main/java/com/antonio/crud/controllers/LoginController.java
@@ -3,11 +3,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.antonio.crud.controllers;
+// com.antonio.crud.controllers.LoginController
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Professor
  */
 public class LoginController {
-    
+    @FXML
+    private TextField txtLogin;
+    @FXML
+    private PasswordField txtSenha;
+    @FXML
+    private Label lblMessage;
+    private Stage dialogStage;
+    private boolean loginSuccess = false;
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+    public boolean isLoginSuccess() {
+        return loginSuccess;
+    }
+    @FXML
+    private void handleLogin(){
+        System.out.println("Fazer login");
+    }
+    @FXML
+    private void handleCancelar(){
+        System.out.println("Saindo do sistema");
+        dialogStage.close();
+    }
 }
